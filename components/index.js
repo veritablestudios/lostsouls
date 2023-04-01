da71491232f4502253bba60ae55965bf49207f50
kmarahar <da11@iitbbs.ac.in>
2023-04-02 03:31:36 +0530

    top sellers

--- components/index.js
+++ components/index.js
@@ -2,4 +2,5 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Button from "./Button";
 import Banner from "./Banner";
-export { Navbar, Footer, Button, Banner };
+import CreatorCard from "./CreatorCard";
+export { Navbar, Footer, Button, Banner, CreatorCard };
