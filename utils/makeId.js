793845ae8e9aabf7a24eb24dbc31290615a512d2
kmarahar <da11@iitbbs.ac.in>
2023-04-03 07:03:20 +0530

    id using nanoid

--- utils/makeId.js
+++ utils/makeId.js
@@ -1,9 +1,6 @@
+import { nanoid } from "nanoid";
+
 export const makeId = (length) => {
-  let result = "";
-  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+  const id = nanoid(length);
+  return id;
 };
