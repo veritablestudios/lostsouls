e1dcf0593c82e64faaa0dc50f3538babaf2a0671
kmarahar <da11@iitbbs.ac.in>
2023-06-04 20:40:25 +0530

    some classes

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -8,8 +8,8 @@ const Banner = ({ parentStyles, name, childStyles }) => {
       >
         {name}
       </p>
-      <div className="absolute w-48 h-48 sm:w-32 sm:h-32 rounded-full white-bg -top-9 -left-16 -z-5" />
-      <div className="absolute w-72 h-72 sm:w-56 sm:h-56 rounded-full white-bg -bottom-24 -right-14 -z-5" />
+      <div className="absolute w-48 h-48 sm:w-32 sm:h-32 rounded-full bg-white/20 -top-9 -left-16 -z-5" />
+      <div className="absolute w-72 h-72 sm:w-56 sm:h-56 rounded-full bg-white/20 -bottom-24 -right-14 -z-5" />
     </div>
   );
 };
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -196,7 +196,7 @@ const Navbar = () => {
           />
         )}
         {isOpen && (
-          <div className="fixed inset-0 top-65 bg-neutral-900 z-10 nav-h flex justify-between flex-col mt-2">
+          <div className="fixed inset-0 top-65 bg-neutral-900 z-10 flex justify-between flex-col mt-2">
             <div className="flex-1 p-4">
               <MenuItems
                 active={active}
